ba637f85d03a72c580507a1e74e531d13c6a537f
lakey <lakey@Lakeybc>
2023-10-02 17:04:52 -0500

    giga

---
+++ Chapter 3/src/DoWhile.java
@@ -0,0 +1,16 @@
+
+public class DoWhile {
+	public static void main ( String[] args) {
+		
+		int num = 100;
+		
+		do 
+		{
+			System.out.println( "DoWhile Countup: " +num);
+			num += 10;
+		}
+		while (num <0);
+		
+	}
+
+}
