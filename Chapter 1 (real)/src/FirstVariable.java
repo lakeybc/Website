e9ef2d8295f829ca03b8f1c4de163f43a4c9084c
lakey <lakey@Lakeybc>
2023-10-02 16:56:05 -0500

    Initial Commit

---
+++ Chapter 1 (real)/src/FirstVariable.java
@@ -0,0 +1,12 @@
+
+public class FirstVariable {
+	
+	public static void main ( String[] args) 
+	{
+		String message = "initial value";
+		System.out.println(message);
+		message = "modified value";
+		System.out.println(message);
+	}
+
+}
